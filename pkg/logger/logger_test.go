f3847ac7380b58859f9fd91dc6635e8d82c4a1f5
KenFujimoto12 <xkenshirou@icloud.com>
2022-01-07 16:52:30 +0900

    update: /pkg/logger test

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -4,6 +4,7 @@
 package logger
 
 import (
+	"bytes"
 	"mxtransporter/pkg/errors"
 	"os"
 	"testing"
@@ -30,7 +31,7 @@ func Test_New(t *testing.T) {
 		runner func(t *testing.T)
 	}{
 		{
-			name: "Check that the log saved to a file correctly. Info level logs should be output with default setting.",
+			name: "Check that a log is saved to a file correctly. Info level logs should be output with default setting.",
 			runner: func(t *testing.T) {
 				l := New(logCfg)
 				l.Info("test log")
@@ -50,7 +51,7 @@ func Test_New(t *testing.T) {
 			},
 		},
 		{
-			name: "Check that the log saved to a file correctly.",
+			name: "Check that a log is saved to a file correctly.",
 			runner: func(t *testing.T) {
 				logCfg.Level = "0"
 				l := New(logCfg)
@@ -71,7 +72,7 @@ func Test_New(t *testing.T) {
 			},
 		},
 		{
-			name: "Check that the json log output correctly.",
+			name: "Pass to output a json log to file correctly.",
 			runner: func(t *testing.T) {
 				logCfg.Level = "1"
 				l := New(logCfg)
@@ -97,7 +98,7 @@ func Test_New(t *testing.T) {
 			},
 		},
 		{
-			name: "Check that the console log output correctly.",
+			name: "Pass to output a console log to file correctly.",
 			runner: func(t *testing.T) {
 				logCfg.Level = "1"
 				logCfg.Format = "console"
@@ -120,6 +121,36 @@ func Test_New(t *testing.T) {
 				l.Error("test log")
 				if lb, _ := os.ReadFile(lf); len(lb) == 0 {
 					t.Fatal("Failed to save error log to log file.")
+				}
+			},
+		},
+		{
+			name: "Pass to output a log to stdout.",
+			runner: func(t *testing.T) {
+				stdout := os.Stdout
+
+				r, w, err := os.Pipe()
+				if err != nil {
+					t.Fatal("Failed to create pipe of w/r file.")
+				}
+
+				os.Stdout = w
+
+				logCfgDefault := Log{}
+				l := New(logCfgDefault)
+				l.Info("test log")
+
+				w.Close()
+
+				os.Stdout = stdout
+
+				var buf bytes.Buffer
+				buf.ReadFrom(r)
+
+				// Even if it is not written to the stdout, 16 characters will be passed when passing the value from the reader of os.Pipe () to the buffer.
+				// so make the following conditions.
+				if len(buf.String()) < 17 {
+					t.Fatal("Failed to save a log into stdout.")
 				}
 			},
 		},
