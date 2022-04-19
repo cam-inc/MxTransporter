6685caab0c70697b7f1e713052b9379f20775c19
KenFujimoto12 <xkenshirou@icloud.com>
2022-04-19 19:01:07 +0900

    fix:Readme and error package

--- interfaces/storage/s3.go
+++ interfaces/storage/s3.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/cam-inc/mxtransporter/pkg/client"
+	"github.com/cam-inc/mxtransporter/pkg/errors"
 	"io"
 )
 
@@ -24,7 +25,7 @@ func (s *s3Cli) GetObject(ctx context.Context, key string) ([]byte, error) {
 	}
 	output, err := s.client.GetObject(ctx, input)
 	if err != nil {
-		return nil, err
+		return nil, errors.InternalServerErrorS3GetObject.Wrap("Failed to get object.", err)
 	}
 	defer output.Body.Close()
 	return io.ReadAll(output.Body)
@@ -38,13 +39,13 @@ func (s *s3Cli) PutObject(ctx context.Context, key, value string) error {
 		Body:   rtBuf,
 	}
 	_, err := s.client.PutObject(ctx, input)
-	return err
+	return errors.InternalServerErrorS3PutObject.Wrap("Failed to put object.", err)
 }
 
 func newS3(ctx context.Context, bucket, region string) (StorageClient, error) {
 	cli := &s3Cli{}
 	if s3Client, err := client.NewS3Client(ctx); err != nil {
-		return nil, err
+		return nil, errors.InternalServerErrorS3NewClient.Wrap("Failed to initialize s3 client.", err)
 	} else {
 		cli.client = s3Client
 	}
